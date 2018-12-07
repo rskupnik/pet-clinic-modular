39ab01b6a3e17eb5b9a3e41f3ed0163acb632584
Myzreal <rskupnik@gmail.com>
2018-12-07 09:26:55 +0100

    Add body to the DefaultPetService

--- com.github.rskupnik.petclinicmodular.application/src/main/java/com/github/rskupnik/petclinicmodular/service/DefaultPetService.java
+++ com.github.rskupnik.petclinicmodular.application/src/main/java/com/github/rskupnik/petclinicmodular/service/DefaultPetService.java
@@ -1,4 +1,25 @@
 package com.github.rskupnik.petclinicmodular.service;
 
-public class DefaultPetService {
+import com.github.rskupnik.petclinicmodular.pet.PetRepository;
+import com.github.rskupnik.petclinicmodular.pet.domain.Pet;
+
+import java.util.List;
+
+public class DefaultPetService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public DefaultPetService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public List<Pet> getAll() {
+        return petRepository.getAll();
+    }
+
+    @Override
+    public void add(Pet pet) {
+        petRepository.add(pet);
+    }
 }
