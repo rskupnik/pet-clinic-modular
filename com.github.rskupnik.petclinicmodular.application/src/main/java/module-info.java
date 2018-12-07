9973c300e5b64a8f4138a001cfb1197cd9c9706f
Myzreal <rskupnik@gmail.com>
2018-12-07 09:47:34 +0100

    Add exports clause to application module

--- com.github.rskupnik.petclinicmodular.application/src/main/java/module-info.java
+++ com.github.rskupnik.petclinicmodular.application/src/main/java/module-info.java
@@ -1,3 +1,6 @@
 module com.github.rskupnik.petclinicmodular.application {
     requires transitive com.github.rskupnik.petclinicmodular.pet;
+
+    exports com.github.rskupnik.petclinicmodular.service;
+    exports com.github.rskupnik.petclinicmodular.service.defaultimpl;
 }
