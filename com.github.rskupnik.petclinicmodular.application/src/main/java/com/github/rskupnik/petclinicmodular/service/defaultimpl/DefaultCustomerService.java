650de8bf459cf525aa888b2dd86c67e28a304492
Myzreal <rskupnik@gmail.com>
2018-12-07 11:15:26 +0100

    Add CustomerService implementation

--- com.github.rskupnik.petclinicmodular.application/src/main/java/com/github/rskupnik/petclinicmodular/service/defaultimpl/DefaultCustomerService.java
+++ com.github.rskupnik.petclinicmodular.application/src/main/java/com/github/rskupnik/petclinicmodular/service/defaultimpl/DefaultCustomerService.java
@@ -1,4 +1,31 @@
 package com.github.rskupnik.petclinicmodular.service.defaultimpl;
 
-public class DefaultCustomerService {
+import com.github.rskupnik.petclinicmodular.customer.domain.Customer;
+import com.github.rskupnik.petclinicmodular.customer.repository.CustomerRepository;
+import com.github.rskupnik.petclinicmodular.service.CustomerService;
+
+import java.util.List;
+
+public class DefaultCustomerService implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public DefaultCustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer get(long id) {
+        return customerRepository.get(id);
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return customerRepository.getAll();
+    }
+
+    @Override
+    public void add(Customer customer) {
+        customerRepository.add(customer);
+    }
 }
--- com.github.rskupnik.petclinicmodular.application/src/main/java/module-info.java
+++ com.github.rskupnik.petclinicmodular.application/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.github.rskupnik.petclinicmodular.application {
     requires transitive com.github.rskupnik.petclinicmodular.pet;
+    requires transitive com.github.rskupnik.petclinicmodular.customer;
 
     exports com.github.rskupnik.petclinicmodular.service;
     exports com.github.rskupnik.petclinicmodular.service.defaultimpl;
