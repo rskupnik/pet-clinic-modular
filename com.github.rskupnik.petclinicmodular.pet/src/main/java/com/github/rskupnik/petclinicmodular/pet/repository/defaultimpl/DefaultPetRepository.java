99cf91487a1dcbac6860937d34b7fe4dcee5f7f5
Myzreal <rskupnik@gmail.com>
2018-12-07 10:09:57 +0100

    Add nextId functionality to default repo

--- com.github.rskupnik.petclinicmodular.pet/src/main/java/com/github/rskupnik/petclinicmodular/pet/repository/defaultimpl/DefaultPetRepository.java
+++ com.github.rskupnik.petclinicmodular.pet/src/main/java/com/github/rskupnik/petclinicmodular/pet/repository/defaultimpl/DefaultPetRepository.java
@@ -9,6 +9,12 @@ public class DefaultPetRepository implements PetRepository {
 
     private final Map<Long, Pet> storage = new HashMap<>();
 
+    private long currentId = 0;
+
+    private long nextId() {
+        return ++currentId;
+    }
+
     @Override
     public Collection<Pet> getAll() {
         return Collections.unmodifiableCollection(storage.values());
@@ -21,8 +27,8 @@ public class DefaultPetRepository implements PetRepository {
 
     @Override
     public void add(Pet pet) {
-        if (pet.getId() != null) {
-            storage.put(pet.getId(), pet);
-        }
+        long id = nextId();
+        pet.setId(id);
+        storage.put(id, pet);
     }
 }
